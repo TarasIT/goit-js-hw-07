43a54f920c90523429c2d8f9db295d8e0004518c
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-09 17:19:17 +0300

    1st task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryList = document.querySelector("div.gallery");
+let originalImage;
 
-const previewImagesGrid = galleryItems
+const imagesGrid = galleryItems
   .map((galleryItem) => {
     const { preview, original, description } = galleryItem;
     return `<div class="gallery__item"><a class="gallery__link" href=${original}><img class= "gallery__image" src=${preview} data-source=${original} alt="${description}" width = 340></a></div>`;
   })
   .join("");
-galleryList.innerHTML = previewImagesGrid;
+galleryList.innerHTML = imagesGrid;
 
-const originalImages = document.querySelectorAll("img.gallery__image");
-const originalImage = document.querySelector("img.gallery__image");
-console.log(originalImages);
-console.log(originalImage.dataset.source);
-
-const selectImage = (event) => {
+function selectImage(event) {
   if (event.target.nodeName !== "IMG") {
     return;
   }
   event.preventDefault();
-};
+
+  originalImage = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="1280">`
+  );
+
+  galleryList.addEventListener("keydown", closeImageByEsc);
+  return originalImage.show();
+}
+
+function closeImageByEsc(event) {
+  if (event.code === "Escape") {
+    galleryList.removeEventListener("keydown", closeImageByEsc);
+    return originalImage.close();
+  }
+}
 
 galleryList.addEventListener("click", selectImage);
